cf6564660a95e6e2c4a16389bc82c48378c3fa70
Kinzerskiy <kinzzersky@gmail.com>
2023-08-17 12:51:37 +0300

    Initial Commit

---
+++ test_roulette/test_rouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_rouletteApp.swift
+//  test_roulette
+//
+//  Created by Mac Pro on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct test_rouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
